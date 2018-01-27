e3efa45dc6e46789f707ca79579f0d807e082c6f
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2018-01-26 22:12:26 -0500

    Configuracion inicial de system y cpu

--- chonky-start.sh
+++ chonky-start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#
 # Script para ejecutar el tema Chonky de Conky
+#
 sleep 2
 conky -c /home/$USER/.conky/chonky/.conkyrc-system &
 conky -c /home/$USER/.conky/chonky/.conkyrc-cpu &
-conky -c /home/$USER/.conky/chonky/.conkyrc-process &
-conky -c /home/$USER/.conky/chonky/.conkyrc-memory &
-conky -c /home/$USER/.conky/chonky/.conkyrc-disk &
-conky -c /home/$USER/.conky/chonky/.conkyrc-network &
+#conky -c /home/$USER/.conky/chonky/.conkyrc-process &
+#conky -c /home/$USER/.conky/chonky/.conkyrc-memory &
+#conky -c /home/$USER/.conky/chonky/.conkyrc-disk &
+#conky -c /home/$USER/.conky/chonky/.conkyrc-network &
 exit
